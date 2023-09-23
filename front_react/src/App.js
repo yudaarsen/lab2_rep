ef82eccb69e13ac64b93e2413e7a7fa14e39157d
Developer2 <dev2@mail.ru>
2023-09-23 14:36:14 +0300

    chore: integration is created

--- front_react/src/App.js
+++ front_react/src/App.js
@@ -1,59 +1,130 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect, createFactory } from 'react';
 
-
-function sendChange() {
-
+function sendDelete(id) {
+  fetch('http://localhost:8080/rest/product/' + id, {
+    method : 'DELETE',
+  }).then();
 }
 
-function sendCreate() {
+function Products(props) {
 
+  function clickHandle(e) {
+    if(!props.flag) {
+      props.setFlagChanged(true);
+      var name;
+      var category;
+      var price;
+      var id;
+      id = e.target.parentElement.id.substring(2);
+      for(var el of e.target.parentElement.children) {
+        if(el.getAttribute('name') == 'name')
+          name = el.innerHTML;
+        if(el.getAttribute('name') == 'category')
+          category = el.innerHTML;
+        if(el.getAttribute('name') == 'price')
+          price = el.innerHTML;
+      }
+      props.setName(name);
+      props.setCategory(category);
+      props.setPrice(price);
+      props.setId(id);
+    }
+  }
+
+  function clickDelete(e) {
+    props.setFlagChanged(false);
+    sendDelete(e.target.parentElement.id.substring(2));
+  }
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch('http://localhost:8080/rest/products')
+    .then(response => response.json())
+    .then(data => {
+      var result = [];
+      for(var it of data) {
+        result.push((
+          <div id={'id' + it.id}>
+            <span>{it.id}</span>
+            <span name="name">{it.name}</span>
+            <span name="category">{it.category}</span>
+            <span name="price">{it.price}</span>
+            <button onClick={(e) => clickDelete(e)}>Удалить</button>
+            <button onClick={(e) => clickHandle(e)}>Изменить</button>
+          </div>
+          )
+          );
+      }
+      setData(result);
+    })
+  }, []);
+
+  
+  return <>{data}</>;
 }
 
-function sendDelete(obj) {
-
+function sendChange(product) {
+  fetch('http://localhost:8080/rest/product', {
+      method : 'PATCH',
+      body : JSON.stringify(product),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).then();
 }
 
 function AddButton(props) {
+  function sendCreate(product) {
+    fetch('http://localhost:8080/rest/product', {
+      method : 'POST',
+      body : JSON.stringify(product),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).then();
+  }
+
   function clickHandle() {
     if(props.flag) {
       props.setFlag(false);
-      props.setPropProduct({});
+      sendChange({id : props.id, name : props.name, category : props.category, price : props.price});
+      props.setName();
+      props.setCategory('Фрукты');
+      props.setPrice();
+    } else {
+      sendCreate({name : props.name, category : props.category, price : props.price});
     }
   }
   return <button onClick={clickHandle}>{props.flag ? 'Изменить' : 'Добавить'}</button>
 }
 
-function Card(props) {
-  function clickHandle() {
-    if(!props.flag) {
-      props.setFlagChanged(true);
-      props.setPropProduct({id : props.id, name : props.name, category: props.category, price : props.price});
-    }
-  }
+function Options(props) {
 
-  function clickDelete() {
-    sendDelete({id : props.id});
-    props.setPropProduct({});
-    props.setFlagChanged(false);
-  }
+  const [cat, setCat] = useState();
 
-  return (
-    <div>
-      <span>{props.id}</span>
-      <span>{props.name}</span>
-      <span>{props.category}</span>
-      <span>{props.price}</span>
-      <button onClick={clickDelete}>Удалить</button>
-      <button onClick={clickHandle}>Изменить</button>
-    </div>
-  );
+  useEffect(() => {
+    fetch('http://localhost:8080/rest/categories', {}).then(response => response.json())
+      .then(data => {
+        var result = [];
+        for(var cat of data) {
+          result.push(<option>{cat}</option>);
+        }
+        setCat(result);
+      });
+  }, []);
+
+  return <>{cat}</>;
 }
 
 function App() {
 
-  const [propProduct, setPropProduct] = useState({});
   const [flagChange, setFlagChanged] = useState();
+  const [name, setName] = useState();
+  const [category, setCategory] = useState('Фрукты');
+  const [price, setPrice] = useState();
+  const [id, setId] = useState();
 
   return (
     <div>
@@ -61,26 +132,25 @@ function App() {
         <div>
           <div>
             <label for="name">Название:</label>  
-            <input value={propProduct.name != null ? propProduct.name : ''} id="name" type="text"></input>
+            <input onChange={(e) => setName(e.target.value)} value={name} id="name" type="text"></input>
           </div>
           <div>
             <label for="category">Категория</label>
-            <select value={propProduct.category != null ? propProduct.category : ''} id="category" datalist="categories">
-              <option>opt1</option>
-              <option>opt2</option>
+            <select onChange={(e) => setCategory(e.target.value)} value={category} id="category" datalist="categories">
+              <Options/>
             </select>
           </div>
           <div>
             <label for="price">Цена:</label>  
-            <input value={propProduct.price != null ? propProduct.price : ''} id="price" type="number" min="1" step="any"></input>
+            <input onChange={(e) => setPrice(e.target.value)} value={price} id="price" type="number" min="1" step="any"></input>
           </div>
           <div>
-            <AddButton flag={flagChange} setFlag={setFlagChanged} setPropProduct={setPropProduct}/>
+            <AddButton flag={flagChange} setFlag={setFlagChanged} setName={setName} name={name} setCategory={setCategory} category={category} setPrice={setPrice} price={price} setId={setId} id={id}/>
           </div>
         </div>
 
         <div>
-          <Card id="1" name="abc" category="MILK" price="123456" setFlagChanged={setFlagChanged} setPropProduct={setPropProduct}/>
+          <Products setFlagChanged={setFlagChanged} setName={setName} name={name} setCategory={setCategory} category={category} setPrice={setPrice} price={price} setId={setId} id={id}/>
         </div>
       </div>
     </div>
