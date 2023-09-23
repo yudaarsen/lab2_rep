f09e64d25ad05008d9e7165c07637a3cdc9c26b8
Developer2 <dev2@mail.ru>
2023-09-23 11:59:11 +0300

    chore: actions are added

--- front_react/src/App.js
+++ front_react/src/App.js
@@ -6,26 +6,53 @@ function sendChange() {
 
 }
 
+function sendCreate() {
+
+}
+
+function sendDelete(obj) {
+
+}
+
 function AddButton(props) {
-  return <button onClick={() => props.flag ? props.setFlag(false) : sendChange()}>{props.flag ? 'Изменить' : 'Добавить'}</button>
+  function clickHandle() {
+    if(props.flag) {
+      props.setFlag(false);
+      props.setPropProduct({});
+    }
+  }
+  return <button onClick={clickHandle}>{props.flag ? 'Изменить' : 'Добавить'}</button>
 }
 
 function Card(props) {
+  function clickHandle() {
+    if(!props.flag) {
+      props.setFlagChanged(true);
+      props.setPropProduct({id : props.id, name : props.name, category: props.category, price : props.price});
+    }
+  }
+
+  function clickDelete() {
+    sendDelete({id : props.id});
+    props.setPropProduct({});
+    props.setFlagChanged(false);
+  }
+
   return (
     <div>
       <span>{props.id}</span>
       <span>{props.name}</span>
       <span>{props.category}</span>
       <span>{props.price}</span>
-      <button>Удалить</button>
-      <button onClick={() => props.setFlagChanged(true)}>Изменить</button>
+      <button onClick={clickDelete}>Удалить</button>
+      <button onClick={clickHandle}>Изменить</button>
     </div>
   );
 }
 
 function App() {
 
-  const [propProduct, setPropProduct] = useState();
+  const [propProduct, setPropProduct] = useState({});
   const [flagChange, setFlagChanged] = useState();
 
   return (
@@ -34,26 +61,26 @@ function App() {
         <div>
           <div>
             <label for="name">Название:</label>  
-            <input id="name" type="text"></input>
+            <input value={propProduct.name != null ? propProduct.name : ''} id="name" type="text"></input>
           </div>
           <div>
             <label for="category">Категория</label>
-            <select id="category" datalist="categories">
+            <select value={propProduct.category != null ? propProduct.category : ''} id="category" datalist="categories">
               <option>opt1</option>
               <option>opt2</option>
             </select>
           </div>
           <div>
             <label for="price">Цена:</label>  
-            <input id="price" type="number" min="1" step="any"></input>
+            <input value={propProduct.price != null ? propProduct.price : ''} id="price" type="number" min="1" step="any"></input>
           </div>
           <div>
-            <AddButton flag={flagChange} setFlag={setFlagChanged}/>
+            <AddButton flag={flagChange} setFlag={setFlagChanged} setPropProduct={setPropProduct}/>
           </div>
         </div>
 
         <div>
-          <Card id="1" name="abc" category="MILK" price="123456" setFlagChanged={setFlagChanged}/>
+          <Card id="1" name="abc" category="MILK" price="123456" setFlagChanged={setFlagChanged} setPropProduct={setPropProduct}/>
         </div>
       </div>
     </div>
